97d288ef9db310ecd8be20c0fe605a9aab4820f7
crico2 <crico@ingerencia.cl>
2026-03-04 20:20:36 -0500

    Initial Commit

---
+++ ResumeBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ResumeBar
+//
+//  Created by Cesar Rico Otero on 4/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ResumeBar/ResumeBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeBarApp.swift
+//  ResumeBar
+//
+//  Created by Cesar Rico Otero on 4/03/26.
+//
+
+import SwiftUI
+
+@main
+struct ResumeBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
